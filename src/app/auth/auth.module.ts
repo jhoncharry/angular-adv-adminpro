024f3d6a3852104290043fc4387f92763bb75ef1
Jhon <jhon.charryserrato@gmail.com>
2020-10-30 23:49:18 -0500

    Fin seccion 7

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+
+
 
 
 
@@ -17,7 +20,9 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule
   ]
 })
 export class AuthModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+
 
 
 
@@ -19,6 +21,9 @@ import { HeaderComponent } from './header/header.component';
     SidebarComponent,
     HeaderComponent
   ],
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
 })
 export class SharedModule { }
