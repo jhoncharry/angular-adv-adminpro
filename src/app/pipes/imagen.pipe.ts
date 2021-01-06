3c143791350bcff728beac69939b93e70429810c
Jhon <jhon.charryserrato@gmail.com>
2021-01-06 00:10:28 -0500

    Ultimos cambios

--- src/app/pipes/imagen.pipe.ts
+++ src/app/pipes/imagen.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 const base_url = environment.base_url;
 
